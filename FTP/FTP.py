30d2df54262d49b11dd30aa27b91bbfa772a9406
pazuzu71 <genchez.tula@gmail.com>
2022-01-22 18:11:41 +0300

    1. Date_now added 2. Make Temp added 3. Clear Temp added

--- FTP/FTP.py
+++ FTP/FTP.py
@@ -1,6 +1,18 @@
 from ftplib import FTP
+from _datetime import datetime
 import os
 
+'''Сохраняем время запуска в Date_now'''
+Date_now = datetime.now()
+'''Проверяем наличие в текущем каталоге временной папки и если такой нет - создаем'''
+try:
+    os.mkdir('Temp')
+except FileExistsError:
+    pass
+'''Проверяем наличие файлов во временной папке, если есть - удаляем'''
+for path, dirs, files in os.walk('Temp'):
+    for file in files:
+        os.unlink(os.path.join(path, file))
 
 ftp = FTP('ftp.zakupki.gov.ru')
 ftp.login('free', 'free')
