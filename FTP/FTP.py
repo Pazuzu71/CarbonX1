edc1feadf5b80f2288499de6e9de5e56c150ef3f
pazuzu71 <genchez.tula@gmail.com>
2022-01-22 18:24:30 +0300

    Config as dictionary from json-file is added

--- FTP/FTP.py
+++ FTP/FTP.py
@@ -1,4 +1,5 @@
 from ftplib import FTP
+import json
 from _datetime import datetime
 import os
 
@@ -13,6 +14,10 @@ except FileExistsError:
 for path, dirs, files in os.walk('Temp'):
     for file in files:
         os.unlink(os.path.join(path, file))
+'''Загружаем в словарь конфиг из джсончика'''
+with open('config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
 
 ftp = FTP('ftp.zakupki.gov.ru')
 ftp.login('free', 'free')
