d433002d0b1d4adf1bd9c0c855cab2075c06fc7f
pazuzu71 <genchez.tula@gmail.com>
2022-01-22 20:09:15 +0300

    The last run date was taken

--- FTP/FTP.py
+++ FTP/FTP.py
@@ -1,10 +1,13 @@
 from ftplib import FTP
 import json
-from _datetime import datetime
+from datetime import datetime
 import os
 
 '''Сохраняем время запуска в Date_now'''
-Date_now = datetime.now()
+Date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+# print(Date_now,type(Date_now))
+
+
 '''Проверяем наличие в текущем каталоге временной папки и если такой нет - создаем'''
 try:
     os.mkdir('Temp')
@@ -17,7 +20,10 @@ for path, dirs, files in os.walk('Temp'):
 '''Загружаем в словарь конфиг из джсончика'''
 with open('config.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
-
+'''Берем из конфига дату последнего запуска'''
+Last_run_time = config['Last_run_time']
+# print(Last_run_time,type(Last_run_time))
+# print(datetime.strptime(Last_run_time, "%Y-%m-%d %H:%M:%S"))
 
 ftp = FTP('ftp.zakupki.gov.ru')
 ftp.login('free', 'free')
