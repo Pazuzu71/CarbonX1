167e290bb9b364ee85c9be03ada4b2a149e92e4c
pazuzu71 <genchez.tula@gmail.com>
2022-01-23 20:43:16 +0300

    FTP is connected and the list of xmls is taken. Also we can download xmls in Temp now.

--- FTP/FTP.py
+++ FTP/FTP.py
@@ -1,11 +1,15 @@
 from ftplib import FTP
 import json
 from datetime import datetime
+from dateutil import parser
 import os
 
 '''Сохраняем время запуска в Date_now'''
 Date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 # print(Date_now,type(Date_now))
+'''Получаем рабочую папку'''
+Work_dir = os.getcwd()
+# print(os.path.join(work_dir,'Temp'))
 
 
 '''Проверяем наличие в текущем каталоге временной папки и если такой нет - создаем'''
@@ -22,9 +26,24 @@ with open('config.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
 '''Берем из конфига дату последнего запуска'''
 Last_run_time = config['Last_run_time']
-# print(Last_run_time,type(Last_run_time))
-# print(datetime.strptime(Last_run_time, "%Y-%m-%d %H:%M:%S"))
 
+'''------------------------------------'''
+'''Подключаемся к ФТП ЕИС'''
 ftp = FTP('ftp.zakupki.gov.ru')
 ftp.login('free', 'free')
+# Меняем рабочую папку на ФТП
+ftp.cwd('fcs_regions//Tulskaja_obl//contracts//currMonth')
+# Создаем список файлов из рабочей папки ФТП ЕИС
+xmls = list()
+ftp.dir(xmls.append)
+for xml in xmls:
+    tokens = xml.split()
+    name = tokens[8]
+    time_str = tokens[5] + " " + tokens[6] + " " + tokens[7]
+    time = parser.parse(time_str)
+    if os.getcwd() != os.path.join(Work_dir, 'Temp'):
+        os.chdir('Temp')
+    with open(name, 'wb') as f:
+        ftp.retrbinary('RETR ' + name, f.write)
+
 ftp.close()
